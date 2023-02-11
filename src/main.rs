4efb40630a20c57c88e64befa0fc1548e9f40d15
Vladimir <vova.krivosheya.2004@gmail.com>
2023-02-11 13:00:41 +0200

    Add join function

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,37 @@ fn split_bytes_into_7bit_chunks<T:Unsigned + From<u8> + AsPrimitive<u8> + BitSto
     (chunks, buffer_size)
 }
 
+//IN PROGRESS
+
+// fn join_7bit_chunks_into_bytes<T:Unsigned + From<u8> + AsPrimitive<u8> + BitAnd<Output=T>>(chunks: &[T], buffer_bits_filled: usize) -> Vec<u8> {
+//     let bits_size = size_of::<T>() * 8 - 1;
+//     let chunk_size = 7;
+//     let mut bytes = Vec::new();
+//     let mut buffer: T = T::from(0);
+//     let mut buffer_bits_filled = buffer_bits_filled;
+//
+//     for chunk in chunks {
+//         buffer = buffer | (*chunk >> (bits_size - buffer_bits_filled));
+//         bytes.push(buffer.as_());
+//         buffer = *chunk & ((1 << (bits_size - buffer_bits_filled)) - 1);
+//         buffer_bits_filled += chunk_size;
+//
+//         while buffer_bits_filled >= 8 {
+//             bytes.push(buffer.as_());
+//             buffer = buffer >> 8;
+//             buffer_bits_filled -= 8;
+//         }
+//     }
+//
+//     if buffer_bits_filled > 0 {
+//         buffer = buffer << (8 - buffer_bits_filled);
+//         bytes.push(buffer.as_());
+//     }
+//
+//     bytes
+// }
+
+
 fn main(){
 
 }
