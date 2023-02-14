d603d95f5dfe43af23694410ace1cfb14bb3930c
Vladimir <vova.krivosheya.2004@gmail.com>
2023-02-14 20:00:02 +0200

    update tests

--- src/main.rs
+++ src/main.rs
@@ -28,32 +28,32 @@ fn join_7bit_chunks_into_bytes<T: BitStore>(chunks: &[T], bytes: usize) -> Vec<u
 #[test]
 fn test_split_bytes_into_7bit_chunks_u8() {
     let bytes = vec![0b1101_0101,0b1010_1010];
-    let (chunks,remainder) = split_bytes_into_7bit_chunks::<u8>(&bytes);
-    let joined_bytes=join_7bit_chunks_into_bytes(&chunks,remainder);
+    let (chunks, bytes_count) = split_bytes_into_7bit_chunks::<u8>(&bytes);
+    let joined_bytes=join_7bit_chunks_into_bytes(&chunks, bytes_count);
     assert_eq!(chunks, [0b0101_0101, 0b0101_0101, 0b0000_0010]);
     assert_eq!(bytes,joined_bytes);
 
     let bytes = vec![0b0111_1111, 0b1111_1111];
-    let (chunks,remainder) = split_bytes_into_7bit_chunks::<u8>(&bytes);
-    let joined_bytes=join_7bit_chunks_into_bytes(&chunks,remainder);
+    let (chunks, bytes_count) = split_bytes_into_7bit_chunks::<u8>(&bytes);
+    let joined_bytes=join_7bit_chunks_into_bytes(&chunks, bytes_count);
     assert_eq!(chunks, [0b0111_1111, 0b0111_1110, 0b0000_0011]);
     assert_eq!(bytes,joined_bytes);
 
     let bytes = vec![0b0111_1111, 0b1111_1111, 0b0111_1110];
-    let (chunks,remainder) = split_bytes_into_7bit_chunks::<u8>(&bytes);
-    let joined_bytes=join_7bit_chunks_into_bytes(&chunks,remainder);
+    let (chunks, bytes_count) = split_bytes_into_7bit_chunks::<u8>(&bytes);
+    let joined_bytes=join_7bit_chunks_into_bytes(&chunks, bytes_count);
     assert_eq!(chunks, [0b0111_1111, 0b0111_1110, 0b0111_1011, 0b0000_0011]);
     assert_eq!(bytes,joined_bytes);
 
     let bytes = vec![0b0000_0000];
-    let (chunks,remainder) = split_bytes_into_7bit_chunks::<u8>(&bytes);
-    let joined_bytes=join_7bit_chunks_into_bytes(&chunks,remainder);
+    let (chunks, bytes_count) = split_bytes_into_7bit_chunks::<u8>(&bytes);
+    let joined_bytes=join_7bit_chunks_into_bytes(&chunks, bytes_count);
     assert_eq!(chunks, [0b0000_0000, 0b0000_0000]);
     assert_eq!(bytes,joined_bytes);
 
     let bytes = vec![0b1000_0000];
-    let (chunks,remainder) = split_bytes_into_7bit_chunks::<u8>(&bytes);
-    let joined_bytes=join_7bit_chunks_into_bytes(&chunks,remainder);
+    let (chunks, bytes_count) = split_bytes_into_7bit_chunks::<u8>(&bytes);
+    let joined_bytes=join_7bit_chunks_into_bytes(&chunks, bytes_count);
     assert_eq!(chunks, [0b0000_0000, 0b0000_0001]);
     assert_eq!(bytes,joined_bytes);
 }
