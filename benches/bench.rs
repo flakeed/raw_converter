4d28f8d9d7e6c910ca69a1931dec4178dac3df7b
uselessgoddess <dodickgod@gmail.com>
2023-02-22 20:58:34 +0300

    Clean up `bench`

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,12 +1,14 @@
-use criterion::Criterion;
-use raw_converter::old;
+#![feature(pointer_is_aligned)]
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use raw_converter::{old, split_into, Store};
+
+#[repr(align(64))]
+struct Align<const N: usize>([u8; N]);
 
 fn criterion_benchmark(c: &mut Criterion) {
-    #[repr(align(64))]
-    struct Align<const N: usize>([u8; N]);
-
     let bytes = Align([243; 100]);
-    let bytes = &bytes.0[1..];
+    let bytes = &bytes.0[2..];
 
     c.bench_function("old", |b| {
         b.iter(|| old::split_chunks(&bytes));
@@ -14,29 +16,25 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("new(u8)", |b| {
         b.iter(|| {
-            let (_chunks, _valid_up) = split_into::<u8>(&bytes);
-            //bytes == join_chunks(&chunks, valid_up)
+            split_into::<u8>(&bytes);
         });
     });
 
     c.bench_function("new(u16)", |b| {
         b.iter(|| {
-            let (_chunks, _valid_up) = split_into::<u16>(&bytes);
-            //bytes == join_chunks(&chunks, valid_up)
+            split_into::<u16>(&bytes);
         });
     });
 
     c.bench_function("new(u32)", |b| {
         b.iter(|| {
-            let (_chunks, _valid_up) = split_into::<u32>(&bytes);
-            //bytes == join_chunks(&chunks, valid_up)
+            split_into::<u32>(&bytes);
         });
     });
 
     c.bench_function("new(u64)", |b| {
         b.iter(|| {
-            let (_chunks, _valid_up) = split_into::<u64>(&bytes);
-            //bytes == join_chunks(&chunks, valid_up)
+            split_into::<u64>(&bytes);
         });
     });
 }
