b048345124a812ecbde50221745fc9f9d04daf9d
uselessgoddess <dodickgod@gmail.com>
2023-02-25 18:02:38 +0300

    Add bench throughput

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,6 @@
 #![feature(pointer_is_aligned)]
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 use raw_converter::{old, split_into, Store};
 
 #[repr(align(64))]
@@ -10,29 +10,29 @@ fn criterion_benchmark(c: &mut Criterion) {
     let bytes = Align([243; 100]);
     let bytes = &bytes.0[2..];
 
-    c.bench_function("old", |b| {
+    let mut group = c.benchmark_group("split");
+    group.throughput(Throughput::BytesDecimal(bytes.len() as u64));
+
+    group.bench_function("old", |b| {
         b.iter(|| old::split_chunks(&bytes));
     });
 
-    c.bench_function("new(u8)", |b| {
+    group.bench_function("new(u8)", |b| {
         b.iter(|| {
             split_into::<u8>(&bytes);
         });
     });
-
-    c.bench_function("new(u16)", |b| {
+    group.bench_function("new(u16)", |b| {
         b.iter(|| {
             split_into::<u16>(&bytes);
         });
     });
-
-    c.bench_function("new(u32)", |b| {
+    group.bench_function("new(u32)", |b| {
         b.iter(|| {
             split_into::<u32>(&bytes);
         });
     });
-
-    c.bench_function("new(u64)", |b| {
+    group.bench_function("new(u64)", |b| {
         b.iter(|| {
             split_into::<u64>(&bytes);
         });
